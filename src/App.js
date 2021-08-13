059de0538f609c783dba8d171ec56339202b12a2
Hue203 <huenguyenmt.vn@gmail.com>
2021-08-13 11:56:19 +0700

    feature/NewHomePage

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from "react";
 import { BrowserRouter as Switch, Route } from "react-router-dom";
 
-import HomePage from './pages/HomePage'
-import AboutPage from './pages/AboutPage'
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
 import FundingPage from "./pages/FundingPage";
 
 import Navbar from "./components/Navbar";
 import FunDetailRequestInfo from "./components/FunDetailRequestInfo/FunDetailRequestInfo";
+import NewHomePage from "./pages/NewHomePage/NewHomePage";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Switch>
-        <Route path="/" exact component={HomePage} />
+        {/* <Route path="/" exact component={HomePage} /> */}
+        <Route path="/" exact component={NewHomePage} />
         <Route path="/about" exact component={AboutPage} />
         <Route path="/funding" exact component={FundingPage} />
         <Route path="/funDetailPage" exact component={FunDetailRequestInfo} />
---
+++ src/pages/NewHomePage/NewHomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NewHomePage = () => {
+  return (
+    <div className="container">
+      <div></div>
+      <h1>This is new homepage</h1>
+    </div>
+  );
+};
+
+export default NewHomePage;
