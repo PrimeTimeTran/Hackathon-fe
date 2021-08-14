8c0ed30b3c57a8166eaddcefc46ab0e125ead316
Vu_Hong_Son <sonvuhong177@gmail.com>
2021-08-14 15:27:24 +0700

    responsive sideBar

--- src/pages/NewHomePage/NewHomePage.js
+++ src/pages/NewHomePage/NewHomePage.js
@@ -9,6 +9,9 @@ import "./style.css";
 import ProviderPetition from "../../components/ProviderPetition/ProviderPettion";
 import PaginationBar from "../../components/PaginationBar/PaginationBar";
 import NewSideBar from "../../components/NewSideBar/NewSideBar";
+import NewFooter from "../../components/NewFooter/NewFooter";
+import Footer from "../../components/Footer/Footer";
+
 const NewHomePage = ({ handleClick }) => {
   const [pageNum, setPageNum] = useState(1);
   const loading = useSelector((state) => state.petition.loading);
@@ -30,75 +33,83 @@ const NewHomePage = ({ handleClick }) => {
   const handleClickOnProvider = () => {};
   return (
     <>
-      <Container>
-        <div className="sideBar">
+      <Container className="mt-5">
+        <div className="sideBar col-4 d-lg-none top-Bar ">
           {" "}
-          <NewSideBar />
+          <NewSideBar topBar={"topBar"} />
         </div>
+        <Row>
+          <div className="sideBar col-2 d-none d-lg-block ">
+            {" "}
+            <NewSideBar />
+          </div>
 
-        <div className="mainPage_Section">
-          {loading ? (
-            <div
-              className="d-flex justify-content-center align-items-center"
-              style={{ minHeight: "100vh" }}
-            >
-              <ClipLoader color="#f86c6b" size={150} loading={true} />
-            </div>
-          ) : (
-            <div>
-              <Container>
-                <Tabs
-                  defaultActiveKey="recivers"
-                  id="uncontrolled-tab-example"
-                  className="mb-3 "
-                >
-                  <Tab
-                    eventKey="recivers"
-                    title="Recivers"
-                    className="tab-title"
+          <div className="mainPage_Section col-10">
+            {loading ? (
+              <div
+                className="d-flex justify-content-center align-items-center"
+                style={{ minHeight: "100vh" }}
+              >
+                <ClipLoader color="#f86c6b" size={150} loading={true} />
+              </div>
+            ) : (
+              <div>
+                <Container>
+                  <Tabs
+                    defaultActiveKey="recivers"
+                    id="uncontrolled-tab-example"
+                    className="mb-3 "
                   >
-                    {recivers.newPetitions?.length ? (
-                      <>
-                        {recivers.newPetitions.map((reciver) => (
-                          <ReciverPettion
-                            reciver={reciver}
-                            handleClick={handleClickOnReciver}
-                            key={reciver._id}
-                            className="tab-container"
-                          />
-                        ))}
-                      </>
-                    ) : (
-                      <p>There are no Reciver</p>
-                    )}
-                  </Tab>
-                  <Tab eventKey="providers" title="Providers">
-                    {providers.newPetitions?.length ? (
-                      <>
-                        {providers.newPetitions.map((provider) => (
-                          <ProviderPetition
-                            provider={provider}
-                            handleClick={handleClickOnProvider}
-                            key={provider._id}
-                            className="tab-container"
-                          />
-                        ))}
-                      </>
-                    ) : (
-                      <p>There are no Reciver</p>
-                    )}
-                  </Tab>
-                </Tabs>
-              </Container>
-            </div>
-          )}
-        </div>
-        <PaginationBar
-          pageNum={pageNum}
-          setPageNum={setPageNum}
-          totalPageNum={totalPageNum}
-          loading={loading}
-        />
+                    <Tab
+                      eventKey="recivers"
+                      title="Recivers"
+                      className="tab-title"
+                    >
+                      {recivers.newPetitions?.length ? (
+                        <>
+                          {recivers.newPetitions.map((reciver) => (
+                            <ReciverPettion
+                              reciver={reciver}
+                              handleClick={handleClickOnReciver}
+                              key={reciver._id}
+                              className="tab-container"
+                            />
+                          ))}
+                        </>
+                      ) : (
+                        <p>There are no Reciver</p>
+                      )}
+                    </Tab>
+                    <Tab eventKey="providers" title="Providers">
+                      {providers.newPetitions?.length ? (
+                        <>
+                          {providers.newPetitions.map((provider) => (
+                            <ProviderPetition
+                              provider={provider}
+                              handleClick={handleClickOnProvider}
+                              key={provider._id}
+                              className="tab-container"
+                            />
+                          ))}
+                        </>
+                      ) : (
+                        <p>There are no Reciver</p>
+                      )}
+                    </Tab>
+                  </Tabs>
+                </Container>
+              </div>
+            )}
+          </div>
+          <PaginationBar
+            pageNum={pageNum}
+            setPageNum={setPageNum}
+            totalPageNum={totalPageNum}
+            loading={loading}
+          />
+        </Row>
+        <NewFooter />
+        <Footer />
       </Container>
     </>
   );
