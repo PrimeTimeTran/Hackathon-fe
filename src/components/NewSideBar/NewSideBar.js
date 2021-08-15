6868b3a1440649b7477e97a7f05b0f6f9ebd9e4d
Chuong Nguyen <quyenchuong1998@gmail.com>
2021-08-15 13:17:38 +0700

    update with new backend

--- src/components/NewSideBar/NewSideBar.js
+++ src/components/NewSideBar/NewSideBar.js
@@ -1,11 +1,20 @@
 import React, { useState } from "react";
 import { Button, Card, Col, Modal, Row } from "react-bootstrap";
+import { useDispatch } from "react-redux";
+import { formActions } from "../../redux/actions/form.actions";
+import MainForm from "../Form/MainForm";
 import "./NewSideBar.css";
 
 const NewSideBar = ({ topBar, setAskShow, modalShow, setModalShow }) => {
   const [show, setShow] = useState(false);
-  const [receive, setReceive] = useState(false);
+  const dispatch = useDispatch();
 
+  const [showDonate, setShowDonate] = useState(false);
+  const [receive, setReceive] = useState(false);
+  const handleDonateClose = () => {
+    setShowDonate(false);
+    dispatch(formActions.changePage("index"));
+  };
   const handleClose = () => {
     if (setAskShow) {
       setAskShow(true);
@@ -24,6 +33,12 @@ const NewSideBar = ({ topBar, setAskShow, modalShow, setModalShow }) => {
       setAskShow(false);
     }
     setShow(true);
+  };
+  const handleShowDonate = () => {
+    if (setAskShow) {
+      setAskShow(false);
+    }
+    setShowDonate(true);
   };
   const handleShowReceive = () => {
     if (setAskShow) {
@@ -70,7 +85,11 @@ const NewSideBar = ({ topBar, setAskShow, modalShow, setModalShow }) => {
           <Button variant="light" className="buttonModal">
             Fund
           </Button>
-          <Button variant="light" className="buttonModal">
+          <Button
+            onClick={handleShowDonate}
+            variant="light"
+            className="buttonModal"
+          >
             Food
           </Button>
         </Modal.Footer>
@@ -85,10 +104,26 @@ const NewSideBar = ({ topBar, setAskShow, modalShow, setModalShow }) => {
           <Button variant="light" className="buttonModal">
             Fund
           </Button>
-          <Button variant="light" className="buttonModal">
+          <Button
+            onClick={handleShowDonate}
+            variant="light"
+            className="buttonModal"
+          >
             Food
           </Button>
         </Modal.Footer>
+      </Modal>
+      <Modal
+        show={showDonate}
+        onHide={handleDonateClose}
+        fullscreen="sm-down"
+        contentClassName="form-modal-content"
+        scrollable
+      >
+        <Modal.Body className="d-flex justify-content-center p-0 main-form-modal">
+          <MainForm handleClose={handleDonateClose} />
+        </Modal.Body>
+        {/* <Modal.Footer></Modal.Footer> */}
       </Modal>
     </>
   );
