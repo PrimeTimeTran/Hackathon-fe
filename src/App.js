d0f865f28a9103f8c35112056efe846e2e263d15
Your Name <you@example.com>
2021-08-15 14:11:02 +0700

    merge

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import { BrowserRouter as Switch, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import FundingPage from "./pages/FundingPage";
-
+import FundRequestDetailPage from "../src/pages/FundRequestDetailPage/FundRequestDetailPage";
 import Navbar from "./components/Navbar";
+<<<<<<< HEAD
 import FunDetailRequestInfo from "./components/FunDetailRequestInfo/FunDetailRequestInfo";
 import NewHomePage from "./pages/NewHomePage/NewHomePage";
+=======
+>>>>>>> 58eca10b7880af79e931adc7cba1fc5dce0d1402
 
 function App() {
   return (
@@ -17,8 +20,16 @@ function App() {
         <Route path="/" exact component={HomePage} />
         <Route path="/about" exact component={AboutPage} />
         <Route path="/funding" exact component={FundingPage} />
+<<<<<<< HEAD
         <Route path="/funDetailPage" exact component={FunDetailRequestInfo} />
         <Route path="/newHomePage" exact component={NewHomePage} />
+=======
+        <Route
+          path="/fundRequestDetailPage"
+          exact
+          component={FundRequestDetailPage}
+        />
+>>>>>>> 58eca10b7880af79e931adc7cba1fc5dce0d1402
       </Switch>
     </div>
   );
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,5 +1,12 @@
 import { combineReducers } from "redux";
 
 export default combineReducers({
+<<<<<<< HEAD
+=======
+  toast: ToastReducer,
+  requestDetail: DonationRequestReducer,
+  requests: DonationRequestsReducer,
+  requestReducer: requestsReducer,
+>>>>>>> 58eca10b7880af79e931adc7cba1fc5dce0d1402
   // TODO: add reducers in here
 });
